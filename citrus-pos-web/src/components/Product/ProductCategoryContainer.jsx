67975d8b09c059bf017112ce5ef7862aebef63b1
unknown <gregorisomarivar@gmail.com>
2025-08-14 17:24:04 -0300

    a

--- citrus-pos-web/src/components/Product/ProductCategoryContainer.jsx
+++ citrus-pos-web/src/components/Product/ProductCategoryContainer.jsx
@@ -15,9 +15,19 @@ function ProductCategoryContainer({ productArray = [], openProductModal  }) {
 
   return (
       <div className="space-y-8 w-full p-6 overflow-auto">
+
         {Object.entries(groupedProductsByCategory).map(([categoryName, products]) => (
           <div key={categoryName} className='flex flex-col items-start w-full'>
-            <h2 className="text-xl text-gray-700 font-bold mb-4">{categoryName}</h2>
+
+           {/* PRODUCT CATEGORY TITLE */}
+            <div className="flex items-center w-full mb-4">
+              <h2 className="text-xl text-gray-700 font-bold whitespace-nowrap">
+                {categoryName}
+              </h2>
+              <div className="flex-1 h-px bg-gray-300 ml-4"></div>
+            </div>
+
+            {/* PRODUCT CARDS CONTAINER */}
             <div className="flex flex-wrap gap-4">
               {
                 products.map(product => (
